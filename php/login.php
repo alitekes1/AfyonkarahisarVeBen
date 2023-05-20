2408058deda07c3f0cfdcb0ab76781f8e2d7ff04
Ali Tekeş <ali.tekes@ogr.sakarya.edu.tr>
2023-05-20 16:37:26 +0300

    detaylar sayfası eklendi + diğer küçük düzeltmeler

--- php/login.php
+++ php/login.php
@@ -6,16 +6,17 @@ if (isset($_POST['submit'])) {
   $username = $_POST['email'];
   $password = $_POST['psw'];
     if ($username == $correctusername && $password == $correctpassword) {
-        echo 
-        "<script> alert('Hoşgeldin Ali :)');window.location.href = 'http://alitekes.epizy.com';
         
+        echo 
+        "<script> alert('Hoşgeldin Ali :)');window.location.href = 'http://alitekes.com.tr';
+
         document.getElementById('girisyap').innerHTML = 'Ali';
-                
+
         </script>";     
-        //header("Location: http://alitekes.epizy.com");
-        ////exit();
     } else {
-        echo "<script> alert('Kullanıcı adı veya şifre hatalı. Lutfen tekrar deneyiniz!');window.location.href = 'http://alitekes.epizy.com/loginPage.html';</script>";     
+
+        echo "<script> alert('Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyiniz!');window.location.href = 'http://alitekes.com.tr/loginPage.html';</script>";     
+    
     }
 }
 ?>
